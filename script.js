f218f617d5eb597887fef84aaa0bae6c9c907672
Lucas Magalhães Rodrigues <imlucasmagalhaes@outlook.com>
2024-06-29 10:57:07 -0300

    creating function to move the snake

--- script.js
+++ script.js
@@ -4,9 +4,11 @@ const size = 30 //tamanho da fruta
 //cria a minnha cobra
 const snake = [
     //o valor muda de 30 em 30, pois o tamanho da fruta é 30
-    {x: 200, y:200},
-    {x: 230, y:200},
+    {x:200, y:200},
+    {x:230, y:200},
 ] 
+
+let direction
 
 //desenha a cobra na tela
 const drawSnake = () => {
@@ -15,6 +17,7 @@ const drawSnake = () => {
     snake.forEach((position, index) => {
         //o index serve para mostrar onde está a cabeça da cobra
         if(index == snake.length - 1){
+            //a última posição do array vai ter uma cor diferente do restante da cobra
             ctx.fillStyle = "white"
         }
 
@@ -22,4 +25,35 @@ const drawSnake = () => {
     })
 }
 
-drawSnake()
+//função para capturar o movimento da cobra
+const moveSnake = () => {
+    
+    if(!direction) return //se o valor for vazio, então a cobra fica parada
+
+    const head = snake.at(-1) //captura o último elemento do meu vetor
+
+    if(direction === "right"){
+        snake.push({x:head.x + size, y:head.y})
+    }
+
+    if(direction === "left"){
+        snake.push({x:head.x - size, y:head.y})
+    }
+
+    if(direction === "down"){
+        snake.push({x:head.x, y:head.y + size})
+    }
+
+    if(direction === "up"){
+        snake.push({x:head.x, y:head.y - size})
+    }
+
+    snake.shift()
+}
+
+setInterval(() => {
+    ctx.clearRect(0, 0, 600, 600)
+    
+    drawSnake()
+    moveSnake()
+}, 300)
